436ce69e15f0e958f7f81aff4cc9df574252e8a2
Danik <chapagana@gmail.com>
2022-12-06 15:26:00 +0600

    initial commit

---
+++ internal/data/movies.go
@@ -0,0 +1,16 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
+	Title     string    `json:"title"`
+	Year      string    `json:"year,omitempty"`
+	// Use the Runtime type instead of int32. Note that the omitempty directive will
+	// still work on this: if the Runtime field has the underlying value 0, then it will // be considered empty and omitted -- and the MarshalJSON() method we just made
+	// won't be called at all.
+	Runtime Runtime  `json:"runtime,omitempty"`
+	Genres  []string `json:"genres,omitempty"`
+	Version int32    `json:"version"`
+}
